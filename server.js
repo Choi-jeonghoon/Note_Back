d5d02f2e2258481f5fbb62ab320a614392b9c50b
J.hoon <68211978+Choi-jeonghoon@users.noreply.github.com>
2023-07-31 15:12:24 +0900

    Add:RoomWebsocket

--- server.js
+++ server.js
@@ -1,18 +1,49 @@
 const express = require("express");
+const http = require("http");
+const socketIO = require("socket.io");
+const cors = require("cors");
+
 const app = express();
-const port = 3000; // 또는 원하는 포트 번호
+const server = http.createServer(app);
+const io = socketIO(server);
 
-// 미들웨어 설정
-app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
+const PORT = process.env.PORT || 3001;
 
-///TEST
-// 라우트 설정
-app.get("/", (req, res) => {
-  res.send("Hello, Express!");
+app.use(
+  cors({
+    origin: "http://localhost:5173", // 클라이언트의 주소를 여기에 입력해주세요.
+    credentials: true, // 필요에 따라 true 또는 false로 설정하세요.
+  })
+);
+
+const rooms = {}; // 방 정보를 저장할 객체
+
+io.on("connection", (socket) => {
+  console.log("새로운 유저가 연결되었습니다.");
+
+  socket.on("joinRoom", (roomName, username) => {
+    socket.join(roomName);
+    if (!rooms[roomName]) {
+      rooms[roomName] = [];
+    }
+    rooms[roomName].push({
+      username,
+      message: `${username}님이 입장했습니다.`,
+    });
+    io.to(roomName).emit("roomInfo", rooms[roomName]);
+  });
+
+  socket.on("sendMessage", (roomName, username, message) => {
+    const newMessage = { username, message };
+    rooms[roomName].push(newMessage);
+    io.to(roomName).emit("newMessage", newMessage);
+  });
+
+  socket.on("disconnect", () => {
+    console.log("유저가 연결을 끊었습니다.");
+  });
 });
 
-// 서버 시작
-app.listen(port, () => {
-  console.log(`Server is running on http://localhost:${port}`);
+server.listen(PORT, () => {
+  console.log(`서버가 ${PORT}번 포트에서 실행 중입니다.`);
 });
