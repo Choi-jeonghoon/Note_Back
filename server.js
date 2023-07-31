9cf75680b8fbf3e80bf1b026dfff6bf3d2200e23
J.hoon <68211978+Choi-jeonghoon@users.noreply.github.com>
2023-07-31 17:51:15 +0900

    Fix:통신에러

--- server.js
+++ server.js
@@ -11,8 +11,8 @@ const PORT = process.env.PORT || 3001;
 
 app.use(
   cors({
-    origin: "http://localhost:5173", // 클라이언트의 주소를 여기에 입력해주세요.
-    credentials: true, // 필요에 따라 true 또는 false로 설정하세요.
+    origin: "http://localhost:5173",
+    credentials: true,
   })
 );
 
@@ -33,10 +33,22 @@ io.on("connection", (socket) => {
     io.to(roomName).emit("roomInfo", rooms[roomName]);
   });
 
+  socket.on("leaveRoom", (roomName, username) => {
+    socket.leave(roomName);
+    if (rooms[roomName]) {
+      rooms[roomName] = rooms[roomName].filter(
+        (message) => message.username !== username
+      );
+      io.to(roomName).emit("roomInfo", rooms[roomName]);
+    }
+  });
+
   socket.on("sendMessage", (roomName, username, message) => {
     const newMessage = { username, message };
-    rooms[roomName].push(newMessage);
-    io.to(roomName).emit("newMessage", newMessage);
+    if (rooms[roomName]) {
+      rooms[roomName].push(newMessage);
+      io.to(roomName).emit("newMessage", newMessage); // 방에 속한 클라이언트에게만 메시지 전송
+    }
   });
 
   socket.on("disconnect", () => {
